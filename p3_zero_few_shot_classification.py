9c6ba6b67055ad8fea201c3f030d997b0f043753
Rawan Reda <rawan.reda.academia@gmail.com>
2026-06-23 03:28:37 +0300

    refactor p3, remove redundant code and fix bugs

--- p3_zero_few_shot_classification.py
+++ p3_zero_few_shot_classification.py
@@ -1,13 +1,13 @@
-import p2_feature_and_classification
+from p2_feature_and_classification import read_data, print_f1_score_and_classification_model
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments, DataCollatorWithPadding
 from datasets import load_dataset, Dataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, classification_report
 import numpy as np
-
+import torch
 
 file_path = r'..\cw-pack-2026\cw-pack-2026\texts\hansard500.csv'
-df = p2_feature_and_classification.read_data(file_path)
+df = read_data(file_path)
 
 
 X = df['speech']
@@ -40,10 +40,10 @@ classifier = pipeline(
     "zero-shot-classification",
     model=zero_shot_model)
 y_pred = []
-for text in X_train.tolist():
+for text in X_test.tolist():
     y_pred.append(classifier(text, candidate_labels)["labels"][0])
 
-print_f1_score_and_classification_model(y_pred, y_test)
+print_f1_score_and_classification_model(y_test, y_pred, "zero shot")
 
 
 
@@ -51,14 +51,14 @@ print_f1_score_and_classification_model(y_pred, y_test)
 
 model = 'microsoft/Phi-4-mini-instruct'
 tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
+pipeline = pipeline(
    'text-generation',
    model=model,
    tokenizer=tokenizer,
    torch_dtype=torch.bfloat16,
    device_map='auto',
 )
-
+ 
 num_examples =5 
 examples_indices = [0,6,8]
 
@@ -88,72 +88,4 @@ for speech in X_test.tolist():
             break
 
 print("few shot ypred", y_pred)
-print_f1_score_and_classification_model(y_test, y_pred)
-
-
-# model_name = "arnir0/Tiny-LLM"
-# model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# tokenizer.pad_token = tokenizer.eos_token
-
-# def tokenize_input(model_inputs):
-#     tokenized = tokenizer(model_inputs["text"], padding="max_length", truncation=True, max_length=512)
-#     tokenized["label"] = [label2id[label] for label in model_inputs["label"]]
-#     return tokenized
-
-# tokenized_train_dataset = train_dataset.map(tokenize_input, batched=True, remove_columns=train_dataset.column_names)
-# tokenized_test_dataset = test_dataset.map(tokenize_input, batched=True, remove_columns=test_dataset.column_names)
-
-
-# training_args = TrainingArguments(
-#     output_dir="trained_model_args",
-#     num_train_epochs=3,
-#     per_device_train_batch_size=2,
-#     gradient_accumulation_steps=8,
-#     gradient_checkpointing=True,
-#     learning_rate=2e-5,
-#     logging_steps=10,
-#     eval_strategy="epoch",
-#     save_strategy="epoch",
-#     load_best_model_at_end=True,
-# )
-
-
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=tokenized_train_dataset,
-#     eval_dataset=tokenized_test_dataset,
-#     processing_class=tokenizer,
-#     data_collator=DataCollatorWithPadding(tokenizer),
-# )
-
-# trainer.train()
-
-# model.save_pretrained("./fine-tuned-model")
-# tokenizer.save_pretrained("./fine-tuned-model")
-
-# # Make predictions on test set
-# test_encodings = tokenizer(X_test.tolist(), padding="max_length", truncation=True, max_length=512, return_tensors="pt")
-# test_predictions = model(**test_encodings)
-# predicted_labels = np.argmax(test_predictions.logits.detach().numpy(), axis=1)
-
-# print(test_predictions)
-# print(predicted_labels)
-# print("Predictions sample:", [id2label[pred] for pred in predicted_labels[:5]])
-
-# # Convert predicted indices to labels
-# y_pred = [id2label[pred] for pred in predicted_labels]
-
-# # Calculate metrics
-# f1 = f1_score(y_test, y_pred, average='macro', zero_division=0)
-# report = classification_report(y_test, y_pred, zero_division=0)
-# print(f"\nF1 Score: {f1}")
-# print("\nClassification Report:")
-# print(report)
-
-
-
-
-
-    # mode for zero-shot classification https://huggingface.co/cross-encoder/nli-distilroberta-base
+print_f1_score_and_classification_model(y_test, y_pred, "few shot")
